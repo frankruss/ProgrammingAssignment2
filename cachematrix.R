e4ca2bbc00399c35327a575e67ae57f155935a71
frankruss <frankruss.ny@gmail.com>
2017-04-08 09:27:20 -0400

    Update cachematrix.R
    Submission of assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,77 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#---------------------------------------------------------------------------
+# Week 3 Programming Assignment #2 
+# Lexical scoping and caching instead of recalculating
+#
+# Using the functions:
+#
+#   Generate a 3 x 3 matrix
+#   > genmatrix <- matrix(sample(1:9), nrow=3, ncol=3)
+#
+#   Cache that matrix
+#   > cachemat <- makeCacheMatrix(genmatrix)  
+#
+#   Demonstrate retrieval of matrix
+#   > cachemat$get()   
+#
+#   Calculate the inverse
+#   > cacheSolve(cachemat)
+#
+#   Demonstrate retrieval of previous matrix
+#   > cacheSolve(cachemat)
+#
+# FrankRuss
+#---------------------------------------------------------------------------
+
+
+# makeCacheMatrix
+# Establishes functions for get, set, inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachematrix <- NULL
+  
+  set <- function(y) {
+    
+    # <<- allows it to be found in parent environments
+    x <<- y
+    cachematrix <<- NULL
+  }
+  
+  # Simply returns x
+  get <- function() x
+  
+  # Calculates inverse, stores
+  setinverse <- function(inverse) cachematrix <<- inverse
+  
+  # simply returns matrix
+  getinverse <- function() cachematrix
+  
+  # Sets up functions in list
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+#---------------------------------------------------------------------------
+# Uses solve() for inverse of matrix 
+# Or retrieves previous calculation when same request is made
+#---------------------------------------------------------------------------
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Get the inverse of the matrix
+  cachematrix <- x$getinverse()
+  
+  # If we already calculated this
+  if (!is.null(cachematrix)) {
+    
+    # Just retrieve previous
+    
+    message("Retrieving previously calculated cached data")
+    return (cachematrix)
+  }
+  
+  # Otherwise do new calculation
+  newmatrix <- x$get()
+  cachematrix <- solve(newmatrix)
+  x$setinverse(cachematrix)
+  cachematrix
 }
